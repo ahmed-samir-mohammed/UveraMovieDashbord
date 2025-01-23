59df359223d82eeb603982b816fb05e3aa4bd22e
ahmed-samir-mohammed <samierahmed37@gmail.com>
2025-01-23 22:24:05 +0200

    Switch NgIf dirctive to control flow

--- src/app/features/trending/trending.component.ts
+++ src/app/features/trending/trending.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, WritableSignal } from '@angular/core';
+import { Component, inject, WritableSignal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MovieCardComponent } from '../../shared/components/movie-card/movie-card.component';
 import { Store } from '@ngrx/store';
 import {
@@ -8,7 +9,6 @@ import {
   selectTotalPages,
 } from '../../store/trending/selectors';
 import * as MovieActions from '../../store/trending/actions';
-import { NgFor } from '@angular/common';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { signal } from '@angular/core';
@@ -17,75 +17,70 @@ import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-trending',
-  imports: [MovieCardComponent, NgxSpinnerModule, NgxPaginationModule, NgFor],
   standalone: true,
+  imports: [CommonModule, MovieCardComponent, NgxSpinnerModule, NgxPaginationModule],
   template: `
-    <div class="w-[90vw] mx-auto mb-24 lg:mb-8 mt-[8rem] lg:mt-[10rem]">
+    <section class="trending-movies w-[90vw] mx-auto mb-24 lg:mb-8 mt-[8rem] lg:mt-[10rem]">
       <h1 class="text-2xl lg:text-3xl font-bold mb-4 text-white">
         Trending Movies
       </h1>
       @defer (when !loading()) {
-      <div
-        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
-      >
-        @if ((movies()?.length ?? 0) > 0) {
-        <ng-container
-          *ngFor="
-            let movie of movies() || []
-              | paginate
-                : {
-                    itemsPerPage: 20,
-                    currentPage: currentPage(),
-                    totalItems: totalPages()
-                  }
-          "
-        >
-          <app-movie-card [movie]="movie"></app-movie-card>
-        </ng-container>
-        }
-      </div>
-      <div class="flex justify-center mt-8">
-        <pagination-controls
-          class="my-pagination rounded"
-          (pageChange)="onPageChange($event)"
-          [responsive]="true"
-          previousLabel="Previous"
-          nextLabel="Next"
-          screenReaderPaginationLabel="Pagination"
-          screenReaderPageLabel="page"
-        ></pagination-controls>
-      </div>
+        <div class="movie-grid grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          @if ((movies()?.length ?? 0) > 0) {
+            @for (movie of movies() || [] | paginate: {
+              itemsPerPage: 20,
+              currentPage: currentPage(),
+              totalItems: totalPages()
+              
+            }; track movie.id) {
+              <app-movie-card [movie]="movie" />
+            }
+          }
+        </div>
+        <nav class="pagination-container flex justify-center mt-8">
+          <pagination-controls
+            class="my-pagination rounded"
+            (pageChange)="onPageChange($event)"
+            [responsive]="true"
+            previousLabel="Previous"
+            nextLabel="Next"
+            screenReaderPaginationLabel="Pagination"
+            screenReaderPageLabel="page"
+          />
+        </nav>
       }
-    </div>
+    </section>
   `,
-  styles: [],
 })
-export class TrendingComponent {
-  store = inject(Store);
-  spinner = inject(NgxSpinnerService);
-  movies = toSignal(this.store.select(selectTrend)); // To be used in the template
-  loading = toSignal(this.store.select(selectLoading)); // To be used in the template
-  totalPages = toSignal(this.store.select(selectTotalPages)); // To be used in the template
-  currentPage: WritableSignal<number> = signal(1); // To update the current page
+export class TrendingComponent implements OnInit {
+  private readonly store = inject(Store);
+  private readonly spinner = inject(NgxSpinnerService);
+
+  protected readonly movies = toSignal(this.store.select(selectTrend));
+  protected readonly loading = toSignal(this.store.select(selectLoading));
+  protected readonly totalPages = toSignal(this.store.select(selectTotalPages));
+  protected readonly currentPage: WritableSignal<number> = signal(1);
 
   constructor() {
     this.store
       .select(selectCurrentPage)
       .pipe(takeUntilDestroyed())
-      .subscribe((page) => this.currentPage.set(page));
+      .subscribe(page => this.currentPage.set(page));
   }
 
-  ngOnInit() {
-    this.loading()
-      ? this.spinner.show()
-      : timer(1000).subscribe(() => this.spinner.hide());
+  ngOnInit(): void {
+    if (this.loading()) {
+      this.spinner.show();
+    } else {
+      timer(1000).subscribe(() => this.spinner.hide());
+    }
 
     this.store.dispatch(
       MovieActions.loadTrendingMovies({ page: this.currentPage() ?? 1 })
     );
   }
 
-  onPageChange(page: number): void {
+  protected onPageChange(page: number): void {
     this.currentPage.set(page);
     this.store.dispatch(MovieActions.loadTrendingMovies({ page }));
   }
